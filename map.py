39f558108a07aa69e150a1f1163f7ac52df7d7f2
Alastair Flynn <alastairflynn@hotmail.co.uk>
2016-10-05 17:37:38 +0100

    Pre pre alpha
    added movement and collision dectection

--- map.py
+++ map.py
@@ -10,6 +10,7 @@ import pyscroll
 import pyscroll.data
 from pyscroll.group import PyscrollGroup
 
+
 class TiledRenderer(object):
 
     def __init__(self, filename, surface, player):
@@ -36,76 +37,11 @@ class TiledRenderer(object):
         # layers begin with 0, so the layers are 0, 1, and 2.
         # since we want the sprite to be on top of layer 1, we set the default
         # layer for sprites as 2
-        self.group = PyscrollGroup(map_layer=self.map_layer, default_layer=1)
+        self.group = PyscrollGroup(map_layer=self.map_layer, default_layer=0)
 
         player.position = self.map_layer.map_rect.center
 
         self.group.add(player)
-
-    def render_map(self, surface):
-        """ Render our map to a pygame surface
-        Feel free to use this as a starting point for your pygame app.
-        This method expects that the surface passed is the same pixel
-        size as the map.
-
-        Scrolling is a often requested feature, but pytmx is a map
-        loader, not a renderer!  If you'd like to have a scrolling map
-        renderer, please see my pyscroll project.
-        """
-
-        # iterate over all the visible layers, then draw them
-        for layer in self.tmx_data.visible_layers:
-            if isinstance(layer, TiledTileLayer):
-                self.render_tile_layer(surface, layer)
-
-            elif isinstance(layer, TiledObjectGroup):
-                self.render_object_layer(surface, layer)
-
-            elif isinstance(layer, TiledImageLayer):
-                self.render_image_layer(surface, layer)
-
-    def render_tile_layer(self, surface, layer):
-        # deref these heavily used references for speed
-        tw = self.tmx_data.tilewidth
-        th = self.tmx_data.tileheight
-        surface_blit = surface.blit
-
-        # iterate over the tiles in the layer
-        for x, y, image in layer.tiles():
-            surface_blit(image, (x * tw, y * th))
-
-    def render_object_layer(self, surface, layer):
-        # deref these heavily used references for speed
-        draw_rect = pygame.draw.rect
-        draw_lines = pygame.draw.lines
-        surface_blit = surface.blit
-
-        # these colors are used to draw vector shapes,
-        # like polygon and box shapes
-        rect_color = (255, 0, 0)
-        poly_color = (0, 255, 0)
-
-        # iterate over all the objects in the layer
-        for obj in layer:
-            # objects with points are polygons or lines
-            if hasattr(obj, 'points'):
-                draw_lines(surface, poly_color, obj.closed, obj.points, 3)
-
-            # some objects have an image
-            # Tiled calls them "GID Objects"
-            elif obj.image:
-                surface_blit(obj.image, (obj.x, obj.y))
-
-            # draw a rect for everything else
-            # Mostly, I am lazy, but you could check if it is circle/oval
-            # and use pygame to draw an oval here...I just do a rect.
-            else:
-                draw_rect(surface, rect_color,
-                          (obj.x, obj.y, obj.width, obj.height), 3)
-
-    def render_image_layer(self, surface, layer):
-        if layer.image:
-            surface.blit(layer.image, (0, 0))
 
     def draw(self, surface, player):
 
--- player.py
+++ player.py
@@ -12,26 +12,83 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         #self.image = pygame.image.load(os.path.join("resources/sprites/player.png"))
         self.dt = dt
-        self.image = pygame.Surface([32, 64])
+        self.width = 32
+        self.height = 64
+        self.image = pygame.Surface([self.width, self.height])
         self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect()
         self.velocity = list([0, 0])
-        self.acceleration = -200
+        self.acceleration = -350
         self.rect = self.image.get_rect()
+        self.old_rect = pygame.Rect(0, 0, self.width, self.height)
         self.PLAYER_MOVE_SPEED = 200
+        self.PLAYER_JUMP_SPEED = 300
+        self.left = False
+        self.right = False
+        self.up = False
+        self.jumping = True
+        self.collision = False
+        self.collision_list = []
+        self.walls = list()
+        self.map_height = 0
+        self.map_width = 0
+        #pygame.key.set_repeat(int(1000 * self.dt), int(1000 * self.dt))
 
-    def update(self):
-        for event in pygame.event.get():  # event handling loop
-            if event.type == KEYDOWN and (event.key == K_d or event.key == K_RIGHT):
-                self.velocity[0] = self.PLAYER_MOVE_SPEED
-            elif event.type == KEYDOWN and (event.key == K_a or event.key == K_LEFT):
-                self.velocity[0] = -self.PLAYER_MOVE_SPEED
-            else:
-                self.velocity[0] = 0
-            if event.type == KEYDOWN and (event.key == K_w or event.key == K_UP):
-                self.velocity[1] = -self.PLAYER_MOVE_SPEED
+    def update(self, keys):
+        self.left = False
+        self.right = False
+        self.up = False
+        self.collision = False
+        self.collision_list = []
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.right = True
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.left = True
+        if keys[pygame.K_w] or keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+            self.up = True
+
+        if self.right:
+            self.velocity[0] = self.PLAYER_MOVE_SPEED
+        elif self.left:
+            self.velocity[0] = -self.PLAYER_MOVE_SPEED
+        elif not self.right or not self.left:
+            self.velocity[0] = 0
+
+        if self.up and not self.jumping:
+            self.velocity[1] = -self.PLAYER_JUMP_SPEED
+            self.jumping = True
+
         self.velocity[1] -= self.dt * self.acceleration
         self.rect.x += self.dt * self.velocity[0]
         self.rect.y += self.dt * self.velocity[1]
 
+        for wall_num in range(len(self.walls)):
+            if self.rect.colliderect(self.walls[wall_num]):
+                self.collision_list.append(wall_num)
+
+
+        for collision_wall_num in self.collision_list:
+            if (self.rect.x <= self.walls[collision_wall_num].right <= self.old_rect.x) or (
+                    self.rect.right >= self.walls[collision_wall_num].x >= self.old_rect.right):
+                self.rect.x = self.old_rect.x
+
+            if self.rect.bottom >= self.walls[collision_wall_num].y >= self.old_rect.bottom:
+                self.velocity[1] = 0
+                self.rect.y = self.old_rect.y
+                self.jumping = False
+
+        if self.rect.right >= self.map_width or self.rect.x <= 0:
+            self.rect.x = self.old_rect.x
+
+        if self.rect.bottom >= self.map_height:
+            self.rect.y = self.old_rect.y
+
+        self.old_rect.y = self.rect.y
+        self.old_rect.x = self.rect.x
+
+        #if self.collision_list > -1:
+        #    self.jumping = False
+        #    self.rect = self.old_rect
+        #    self.collision = True
 
